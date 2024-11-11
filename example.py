57f2324b0387c2cb273997dbf9bdf4ed395a0d53
Harshawardhan Fartale <harshwardhanfartale.nith@gmail.com>
2024-11-11 15:19:43 +0530

    Division

--- example.py
+++ example.py
@@ -8,8 +8,17 @@ def subtract(x,y):
 def multiply(x,y):
     return x*y
 
+def divide(x,y):
+    if y == 0:
+        return "Error: Division by zero"
+    else:
+        return x/y
+    
+
 print(add(1,2))
 
 print(subtract(1,2))
 
 print(multiply(2,3))
+
+print(divide(4,2))
